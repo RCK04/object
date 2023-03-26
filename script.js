ee90e1302f126c015b3440a41107f28376b36981
Erick Roberto <erickkan2004@gmail.com>
2023-03-25 21:25:42 -0300

    main

---
+++ script.js
@@ -0,0 +1,36 @@
+/*  
+- Orientação a Objetos 
+- Programação Orienta a Objetos
+- Programação Procedural - 1 Código abaixo do outro
+- Programaçao Funcional
+
+- CLASSES
+- FUNÇÕES / OBJETOS
+
+*/
+
+// Template
+// Geralmente Primeira letra maiuscula
+// constructor sempre que for criar uma 'pessoa' nova
+class Person {
+
+    age = 0;
+
+    constructor(name){
+        this.name = name;
+    }
+}
+
+let p1 = new Person('João')
+let p2 = new Person('Maria')
+let p3 = new Person('Pedro')
+
+p1.age = 20;
+p2.age = 30;
+p3.age = 40;
+
+console.log(`P1 = ${p1.name} tem ${p1.age} anos`);
+
+console.log(`P2 = ${p2.name} tem ${p2.age} anos`);
+
+console.log(`P3 = ${p3.name} tem ${p3.age} anos`);
