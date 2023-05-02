a170e8c2fa08e5d2a1366f1c3f20ab36e1df651c
Erick Roberto <erickkan2004@gmail.com>
2023-05-01 23:02:24 -0300

    main

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ class Person {
 
 let p1 = new Person('João') //nome
 let p2 = new Person('Maria') //nome
-let p3 = new Person('Pedro') //nome
+let p3 = new Person('Pedro') //name
 
 p1.age = 20;
 p2.age = 30;
